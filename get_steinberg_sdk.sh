e6c713f8524f5cb790938a2a3975f1ac6434993c
Bram de Jong <bram.dejong@gmail.com>
2016-06-03 23:28:41 +0200

    Trying to get the steinberg SDK without errors...

--- get_steinberg_sdk.sh
+++ get_steinberg_sdk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget http://www.steinberg.net/sdk_downloads/vstsdk365_12_11_2015_build_67.zip
 unzip -q -o vstsdk365_12_11_2015_build_67.zip
-mv -rf VST3\ SDK/* Steinberg/
-rm -rf VST3\ SDK vstsdk365_12_11_2015_build_67*
+mv VST3\ SDK Steinberg
+rm vstsdk365_12_11_2015_build_67*
