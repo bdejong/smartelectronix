0a429d9c3313d21daff27aaf26af2c56c7fc23e0
Bram de Jong <bram.dejong@gmail.com>
2016-06-03 23:17:06 +0200

    Added travis build script

---
+++ get_steinberg_sdk.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget http://www.steinberg.net/sdk_downloads/vstsdk365_12_11_2015_build_67.zip
+unzip -q -o vstsdk365_12_11_2015_build_67.zip
+mv -R VST3\ SDK/* Steinberg/
+rm -rf VST3\ SDK vstsdk365_12_11_2015_build_67*
