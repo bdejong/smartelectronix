4d3083aea977d5444bdf97d89e56934d970b4e6f
Bram de Jong <bram.dejong@gmail.com>
2016-12-20 12:22:24 +0100

    Fixed compatibility with python2 and 3, closed #30

--- get_steinberg_sdk.py
+++ get_steinberg_sdk.py
@@ -3,21 +3,25 @@ from zipfile import ZipFile
 import shutil
 import os
 
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
 
 class ProgressReporter:
     def __init__(self):
         self.progress = None
 
     def __call__(self, block_count, block_size, total):
-        progress = (block_count * block_size * 100) / total
-        progress = (progress / 20) * 20
+        progress = int((block_count * block_size * 100) / total)
+        progress = int(progress / 20) * 20
         if progress != self.progress:
-            print "{}%".format(progress)
+            print("{}%".format(progress))
             self.progress = progress
 
 
 url = "http://www.steinberg.net/sdk_downloads/vstsdk366_27_06_2016_build_61.zip"  # noqa
-urllib.urlretrieve(url, "Steinberg.zip", ProgressReporter())
+urlretrieve(url, "Steinberg.zip", ProgressReporter())
 
 zip = ZipFile('Steinberg.zip')
 zip.extractall()
